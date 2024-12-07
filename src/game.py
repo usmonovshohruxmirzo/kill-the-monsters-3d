97616bcbacdfe992714e419412888016d92b7ab0
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2024-12-07 22:11:49 +0500

    feat(game): oppenents added

--- src/game.py
+++ src/game.py
@@ -10,4 +10,11 @@ player = FirstPersonController()
 player.speed = 5
 player.jump_height = 2
 
+all_opponents = []
+opponents_length = 10
+for _ in range(opponents_length):
+    x, y, z = random.randint(-20, 20), 1, random.randint(-20, 20)
+    opponent = Entity(model='sphere', color=color.yellow, scale=1, position=(x, y, z), collider='sphere')
+    all_opponents.append(opponent)
+
 app.run()
